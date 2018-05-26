ea0421ab9ac57443784c2322d2f9c75c2f6b09b2
Nis Meinert <nis.meinert@gmail.com>
2018-05-26 18:09:33 +0200

    add approve and takeOwnership
    Without the possibility to change ownership of a document an author can effectively block hash values.

--- contracts/documents.sol
+++ contracts/documents.sol
@@ -11,10 +11,13 @@ contract Documents {
     }
 
     mapping(bytes32 => Document) public documents;
+    mapping(bytes32 => address) public approvals;
 
     event DocumentAdded(bytes32 hash, address author);
     event HashUpdated(bytes32 oldHash, bytes32 newHash, address author);
     event RevokeUpdated(bytes32 hash, address author, bool newStatus);
+    event Approval(bytes32 hash, address from, address to);
+    event OwnershipTransfer(bytes32 hash, address from, address to);
     
     modifier onlyAuthor(bytes32 hash) {
         require(documents[hash].author == msg.sender);
@@ -53,5 +56,19 @@ contract Documents {
         }
         
         emit RevokeUpdated(hash, msg.sender, revoke);
+    }
+    
+    function approve(address to, bytes32 hash) public
+        onlyAuthor(hash) {
+        
+        approvals[hash] = msg.sender;
+        emit Approval(hash, msg.sender, to);
+    }
+    
+    function takeOwnership(bytes32 hash) public {
+        require(approvals[hash] == msg.sender);
+        address prevAuthor = documents[hash].author;
+        documents[hash].author = msg.sender;
+        emit OwnershipTransfer(hash, prevAuthor, msg.sender);
     }
 }
