e429684f055d0c9eff316cbc4306875653f5c097
Nis Meinert <nis.meinert@gmail.com>
2018-05-27 17:08:55 +0200

    add --verbose --store

--- contracts/deploy.py
+++ contracts/deploy.py
@@ -2,33 +2,47 @@ import getopt, sys, os
 from web3.auto import w3
 import json
 
+def usage(failure = False):
+    if not failure:
+        print('Deploys contract to web3.py\'s default node '
+              'and prints the address and / or the file name where the address is stored.')
+    else:
+        print('Error: Invalid usage!')
+
+    print('\nAvailable options are:')
+    print('  -h, --help: print help dialog')
+    print('  -v, --verbose: verbose output')
+    print('  -s, --save: store address in file <CONTRACT_NAME>.address')
+    print('  -c<CONTRACT_NAME.sol> --contract=<CONTRACT_NAME.sol>: name of the contract [REQUIRED]')
+    print('  -p<BUILD DIRECTORY>, --path=<BUILD DIRECTORY>: where to find .abi and .bin files [REQUIRED]')
+
 try:
-    opts = getopt.getopt(sys.argv[1:], 'hc:p:', ['help', 'contract=', 'path='])
+    opts = getopt.getopt(sys.argv[1:], 'hvsc:p:', ['help', 'verbose', 'save', 'contract=', 'path='])
 except getopt.GetoptError as e:
     print(str(e))
-    usage()
+    usage(True)
     sys.exit(1)
 
-def usage():
-    print('Available options are:')
-    print('  -h, --help: Print this dialog')
-    print('  -c<MYCONTRACT.sol> --contract=<MYCONTRACT.sol>: name of the contract (required)')
-    print('  -p<BUILD DIRECTORY>, --path=<BUILD DIRECTORY>: where to find .abi and .bin files (required)')
-
 default_account = 0
+verbose = False
+dump_addr = False
 contract_name = None
 build_dir = None
 for opt, arg in opts[0]:
     if opt in ('-h', '--help'):
         usage()
         sys.exit()
+    elif opt in ('-v', '--verbose'):
+        verbose = True
+    elif opt in ('-s', '--save'):
+        dump_addr = True
     elif opt in ('-c', '--contract'):
         contract_name = os.path.basename(os.path.splitext(arg)[0])
     elif opt in ('-p', '--path'):
         build_dir = arg
 
 if not all([contract_name, build_dir]):
-    usage()
+    usage(True)
     exit(1)
 
 
@@ -52,9 +66,15 @@ tx_hash = contract.constructor().transact()
 tx_receipt = w3.eth.waitForTransactionReceipt(tx_hash)
 
 address = tx_receipt.contractAddress
-address_file_name = contract_name + '.address'
-print('Contract address: {}'.format(address))
-print('Stored address in file: {}'.format(address_file_name))
 
-with open(address_file_name, 'w') as f:
-    f.write(address)
+if verbose or not dump_addr:
+    temp = 'Contract address: {}' if verbose else '{}'
+    print(temp.format(address))
+
+if dump_addr:
+    address_file_name = contract_name + '.address'
+    temp = 'Stored address in file: {}' if verbose else '{}'
+    print(temp.format(address_file_name))
+
+    with open(address_file_name, 'w') as f:
+        f.write(address)
